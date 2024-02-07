4761d52ff0491dd8c32e3458edd765712978a9a4
Kagami Rei <90980127+reiify@users.noreply.github.com>
2024-02-07 18:07:55 +0500

    Update InfoPage.vue

--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -3,13 +3,13 @@
         <q-card class="info-card" flat bordered>
             <q-card-section>
                 <div class="text-h6">Diploma work</div>
-                <a href="https://portfolio-krei.vercel.app" class="text-subtitle1">Abdulatif Ahmadov</a>
+                <a href="https://portfolio-krei.vercel.app" class="text-subtitle1 text-black">by Abdulatif Ahmadov</a>
             </q-card-section>
 
             <q-separator inset />
 
             <q-list>
-                <q-item clickable>
+                <q-item href="https://vuejs.org">
                     <q-item-section avatar>
                         <q-avatar square size="24px">
                             <img src="https://vuejs.org/logo.svg">
@@ -20,7 +20,7 @@
                         <q-item-label caption>Framework</q-item-label>
                     </q-item-section>
                 </q-item>
-                <q-item clickable>
+                <q-item href="https://vitejs.dev">
                     <q-item-section avatar>
                         <q-avatar square size="24px">
                             <img src="https://vitejs.dev/logo.svg">
@@ -31,18 +31,29 @@
                         <q-item-label caption>Builder</q-item-label>
                     </q-item-section>
                 </q-item>
-                <q-item clickable>
+                <q-item href="https://quasar.dev">
                     <q-item-section avatar>
                         <q-avatar square size="24px">
                             <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
                         </q-avatar>
                     </q-item-section>
                     <q-item-section>
-                        <q-item-label>Quasar, QNotify</q-item-label>
+                        <q-item-label>Quasar</q-item-label>
+                        <q-item-label caption>Library</q-item-label>
+                    </q-item-section>
+                </q-item>
+                <q-item href="https://quasar.dev">
+                    <q-item-section avatar>
+                        <q-avatar square size="24px">
+                            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
+                        </q-avatar>
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>QNotify</q-item-label>
                         <q-item-label caption>Plugins</q-item-label>
                     </q-item-section>
                 </q-item>
-                <q-item clickable>
+                <q-item href="https://pinia.vuejs.org">
                     <q-item-section avatar>
                         <q-avatar square size="24px">
                             <img src="https://pinia.vuejs.org/logo.svg">
@@ -50,10 +61,10 @@
                     </q-item-section>
                     <q-item-section>
                         <q-item-label>Pinia</q-item-label>
-                        <q-item-label caption>State management</q-item-label>
+                        <q-item-label caption>State manager</q-item-label>
                     </q-item-section>
                 </q-item>
-                <q-item clickable>
+                <q-item href="https://deepl.com">
                     <q-item-section avatar>
                         <q-avatar square size="24px">
                             <img src="https://static.deepl.com/img/logo/deepl-logo-blue.svg">
